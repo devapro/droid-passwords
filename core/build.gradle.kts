fc088cc01b41466c9afd6d3ca13870db071e2b42
Arsenii Kharlanov <arsenyzp@gmail.com>
2025-08-25 16:11:50 +0200

    Added core module

---
+++ core/build.gradle.kts
@@ -0,0 +1,160 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.kotlinSerialization)
+}
+
+kotlin {
+    androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
+    jvm("desktop")
+
+    sourceSets {
+        val desktopMain by getting
+
+        androidMain.dependencies {
+            implementation(compose.preview)
+            implementation(libs.androidx.activity.compose)
+            implementation(project.dependencies.platform(libs.koin.bom))
+            implementation(libs.koin.core)
+            implementation(libs.koin.android)
+            implementation(libs.kstore.file)
+            implementation(libs.cryptography.provider.jdk)
+        }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.material3)
+            implementation(compose.materialIconsExtended)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.kotlinx.uuid)
+
+            // Koin
+            implementation(project.dependencies.platform(libs.koin.bom))
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.viewmodel)
+            implementation(libs.kotlinx.serialization.json)
+
+            // DataStore library
+            implementation(libs.androidx.datastore)
+
+            // The Preferences DataStore library
+            implementation(libs.androidx.datastore.preferences)
+
+            // Navigator
+            implementation(libs.voyager.navigator)
+
+            // TabNavigator
+            implementation(libs.voyager.tab.navigator)
+
+            // Transitions
+            implementation(libs.voyager.transitions)
+
+            // FileKit
+            implementation(libs.filekit.core)
+            // Enables FileKit dialogs without Compose dependencies
+            implementation(libs.filekit.dialogs)
+            // Enables FileKit dialogs with Composable utilities
+            implementation(libs.filekit.dialogs.compose)
+
+            implementation(libs.cryptography.core)
+            implementation(libs.cryptography.provider.optimal)
+
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+        desktopMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(libs.kotlinx.coroutinesSwing)
+            implementation(libs.cryptography.provider.jdk)
+        }
+        iosMain.dependencies {
+            implementation(libs.cryptography.provider.apple)
+        }
+    }
+}
+
+android {
+    namespace = "io.github.devapro.droid.core"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    debugImplementation(compose.uiTooling)
+}
+
+compose.desktop {
+    application {
+        mainClass = "io.github.devapro.MainKt"
+
+        buildTypes.release.proguard {
+            obfuscate = true
+            configurationFiles.from(project.file("compose-desktop.pro"))
+        }
+
+        nativeDistributions {
+            linux {
+                modules("jdk.security.auth")
+                iconFile.set(File("logo/Linux/appIcon.png"))
+            }
+            windows {
+                iconFile.set(File("logo/Windows/appIcon.ico"))
+            }
+            macOS {
+                iconFile.set(File("logo/macOS/appIcon.icns"))
+            }
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "io.github.devapro"
+            packageVersion = "1.0.0"
+        }
+    }
+}
