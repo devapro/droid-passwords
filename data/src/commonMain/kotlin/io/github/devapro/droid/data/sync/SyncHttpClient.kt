7da9674865fbcf594b478f85d86599249ac373e7
Arsenii Kharlanov <arsenyzp@gmail.com>
2026-06-29 19:56:18 +0200

    Fix android app

--- data/src/commonMain/kotlin/io/github/devapro/droid/data/sync/SyncHttpClient.kt
+++ data/src/commonMain/kotlin/io/github/devapro/droid/data/sync/SyncHttpClient.kt
@@ -4,6 +4,8 @@ import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -31,5 +33,8 @@ internal fun HttpClientConfig<*>.installSyncDefaults() {
         requestTimeoutMillis = 30_000
         connectTimeoutMillis = 15_000
         socketTimeoutMillis = 30_000
+    }
+    install(Logging) {
+        level = LogLevel.INFO
     }
 }
